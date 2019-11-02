722493359c60741194758b856a51a55a06b9f53b
Chandra Novaldo <chandranovaldo@Chandras-MacBook-Pro.local>
2019-11-02 15:25:07 +0700

    addExercise4edit

--- Exercise4.js
+++ Exercise4.js
@@ -1,11 +1,12 @@
 var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"]
 
 function dataHandling2(input) {
-    var arrayOutput = [] // bikin array baru untuk menampung hasil
-    //lakukan splice 
+    var arrayOutput = [] 
+
     input.splice(1, 4, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro")
+    
     arrayOutput.push(input)
-    //lakukan switch + split
+    
     var tanggalHuruf = input[3].split('/')
     switch (tanggalHuruf[1]) {
         case "01":
@@ -45,28 +46,26 @@ function dataHandling2(input) {
             tanggalHuruf.splice(1, 1, "Desember");
             break;
     }
-    // console.log(tanggalHuruf[1])
+    
     arrayOutput.push(tanggalHuruf[1])
-    // lakukan split + sort
+    
     var tanggalSort = input[3].split('/')
     tanggalSort.sort(function (value1, value2) { return value2 - value1 })
-    // console.log(tanggalSort)
+    
     arrayOutput.push(tanggalSort)
-    //lakukan split + join
+    
     var tanggalJoin = input[3].split('/')
-    // console.log(tanggalJoin.join('-'))
+    
     arrayOutput.push(tanggalJoin.join('-'))
-    //lakukan slice
+    
     var inputSlice = input.slice(1, 2)
-    // console.log(inputSlice) // dapat hasil ['Roman Alamsyah Elsharawy']
-    // ubah inputSlice menjadi string karena array dengan 1 nilai saja tidak bs dislice
-    // 1 gunakan split
+    
     var namaSplit = inputSlice[0].split(' ')
-    // console.log(namaSplit)
-    var namaString = namaSplit.join(' ') //2 gunakan join
-    // console.log(namaString)
-    var namaSlice = namaString.slice(0, 14) //3 baru dislice
-    // console.log(namaSlice)
+    
+    var namaString = namaSplit.join(' ') 
+    
+    var namaSlice = namaString.slice(0, 14) 
+   
     arrayOutput.push(namaSlice)
 
     return arrayOutput
